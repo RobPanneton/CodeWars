57e32ecd9d14eedf525056640da0cfb13b68679b
RobPanneton <robert.panneton@gmail.com>
2020-12-24 15:59:31 -0500

    add kana folder and a kana

---
+++ Kanas/breakCamcelCase.js
@@ -0,0 +1,17 @@
+function solution(string) {
+  const capRegEx = /^[A-Z]/;
+  let newString = [];
+  let splits = 0;
+  string.split("").forEach((char, index) => {
+    newString.push(char);
+    if (capRegEx.test(char)) {
+      newString.splice(index + splits, 0, " ");
+      splits++;
+    }
+  });
+  return newString.join("");
+}
+
+// console.log(solution("camelCasing"));
+// console.log(solution("camelCasingTest"));
+console.log(solution("camelCasingTestAgain"));
