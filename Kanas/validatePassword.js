379fbae3a76d4965f422eebc8c4baad04712810c
RobPanneton <robert.panneton@gmail.com>
2020-12-25 20:34:57 -0500

    add regex kana

--- Kanas/validatePassword.js
+++ Kanas/validatePassword.js
@@ -9,3 +9,14 @@ function validPass(password) {
     ? "VALID"
     : "INVALID";
 }
+
+// I will give you a string. You respond with "VALID"
+//if the string meets the requirements or "INVALID" if it does not.
+
+// Passwords must abide by the following requirements:
+
+// More than 3 characters but less than 20.
+
+// Must contain only alphanumeric characters.
+
+// Must contain letters and numbers.
