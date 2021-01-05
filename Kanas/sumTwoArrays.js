2293149963ac045842a2eef1a9007c74e7907d8f
RobPanneton <robert.panneton@gmail.com>
2021-01-04 22:07:40 -0500

    add new kana

---
+++ Kanas/sumTwoArrays.js
@@ -0,0 +1,36 @@
+function addArrays(array1, array2) {
+  return !array1.length && !array2.length
+    ? []
+    : Number(array1.join("")) + Number(array2.join("")) >= 0
+    ? (Number(array1.join("")) + Number(array2.join("")))
+        .toString()
+        .split("")
+        .map((d) => Number(d))
+    : (Number(array1.join("")) + Number(array2.join("")))
+        .toString()
+        .split("")
+        .map((d, index) => (index === 1 ? Number(d) * -1 : Number(d)))
+        .splice((0, 1));
+}
+
+//   if (!array1.length && !array2.length) return [];
+
+//   console.log({ array1: array1, array2: array2 });
+
+//   const sum = Number(array1.join("")) + Number(array2.join(""));
+
+//   if (sum >= 0)
+//  return sum
+//    .toString()
+//    .split("")
+//    .map((d) => Number(d));
+
+//   return sum
+//     .toString()
+//     .split("")
+//     .map((d, index) => (index === 1 ? Number(d) * -1 : Number(d)))
+//     .splice((0, 1));
+
+console.log(addArrays([6, 7], [6, 7]));
+console.log(addArrays([], []));
+console.log(addArrays([-6, 7], [1, 0]));
