a869825cf3154aa04d2632c3e13e5d9694e3ad1a
RobPanneton <robert.panneton@gmail.com>
2021-01-06 16:00:37 -0500

    add more kata

---
+++ Kanas/uniqueInOrder.js
@@ -0,0 +1,19 @@
+var uniqueInOrder = function (iterable) {
+  return Array.isArray(iterable)
+    ? iterable.filter((char, index) => char !== iterable[index - 1])
+    : iterable
+        .split("")
+        .filter((char, index) => char !== iterable.charAt(index - 1));
+};
+
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+
+// Implement the function unique_in_order which takes as argument a
+// sequence and returns a list of items without any elements with the same value next to each other
+// and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
