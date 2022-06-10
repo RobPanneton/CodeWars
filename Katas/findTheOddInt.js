172f469154e81e4485c4857d6f9d2513ff8397da
RobPanneton <robert.panneton@gmail.com>
2022-06-09 21:54:56 -0400

    add a couple of solutions

---
+++ Katas/findTheOddInt.js
@@ -0,0 +1,24 @@
+function findOdd(A) {
+  let digitCounter = {};
+
+  A.forEach((int) => {
+    digitCounter[int] = digitCounter[int] ? digitCounter[int]++ : 1;
+  });
+
+  return Object.keys(digitCounter).find((int) => digitCounter[int] % 2 !== 0);
+}
+
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
+console.log(findOdd([10]));
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
